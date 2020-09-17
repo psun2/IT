46597a2ce7ecb19433e1ed425aeaf2997b069d80
박성언 <tjddjs90@naver.com>
2020-09-17 15:24:46 +0900

    DropDown Menu

--- Language/JavaScript/05. function/DropDown Menu/main.js
+++ Language/JavaScript/05. function/DropDown Menu/main.js
@@ -1,9 +1,9 @@
 const projectsBtn = document.querySelector('.projects button'),
   productsBtn = document.querySelector('.products button'),
   projectsUl = document.querySelector('.projects ul'),
-  productsUl = document.querySelector('.products ul'),
-  projectsLi = document.querySelectorAll('.projects ul li'),
-  productsLi = document.querySelectorAll('.products ul li');
+  productsUl = document.querySelector('.products ul');
+
+let currentMenu = undefined;
 
 const mouseleave = () => {
   projectsUl.addEventListener('mouseleave', handleFocus);
@@ -11,36 +11,25 @@ const mouseleave = () => {
 };
 
 const handleClick = (event) => {
+  if (currentMenu) currentMenu.classList.remove('active');
   const target = event.target;
-  const displayTarget = target.nextElementSibling;
-  displayTarget.classList.toggle('active');
+  const displayTarget = target.nextElementSibling; // ul
+  if (currentMenu === displayTarget) return;
+  displayTarget.classList.add('active');
+  currentMenu = displayTarget;
 };
 
 const handleFocus = (event) => {
+  // ul에서 나갔을때
   const target = event.target;
-  target.classList.toggle('active');
-};
-
-const handleClickItem = (event) => {
-  const target = event.target.parentNode.parentNode;
-  projectsUl.removeEventListener('mouseleave', handleFocus);
-  productsUl.removeEventListener('mouseleave', handleFocus);
-  target.classList.toggle('active');
-  setTimeout(() => {
-    mouseleave();
-  }, 100);
+  target.classList.remove('active');
+  currentMenu = undefined;
 };
 
 const init = () => {
   projectsBtn.addEventListener('click', handleClick);
   productsBtn.addEventListener('click', handleClick);
   mouseleave();
-  Array.from(projectsLi).forEach((index) => {
-    index.addEventListener('click', handleClickItem);
-  });
-  Array.from(productsLi).forEach((index) => {
-    index.addEventListener('click', handleClickItem);
-  });
 };
 
 init();
