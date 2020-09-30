c80dc014c461ab6a315e86046116e6ebe6efd6ec
박성언 <tjddjs90@naver.com>
2020-10-01 00:41:16 +0900

    react-native

--- Nomad Coders/10. React Native - 날씨 웹 서비스/fokin-weather/Loading.js
+++ Nomad Coders/10. React Native - 날씨 웹 서비스/fokin-weather/Loading.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
 export default function Loading() {
   return (
     <View style={styles.container}>
+      {/* 
+      앱 시작히 상태바의 글씨가 흰색이여서 안보임을 해소하고자
+      잠시나마 검은색으로 보이게 하기
+      */}
+      <StatusBar barStyle="dark-content" />
       <Text style={styles.text}>Getting the fucking weather</Text>
     </View>
   );
