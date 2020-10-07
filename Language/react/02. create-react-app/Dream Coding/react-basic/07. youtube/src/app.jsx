48ff56cfa65b1c329783c82fc253827d2b643dd2
박성언 <tjddjs90@naver.com>
2020-10-07 21:37:46 +0900

    8.10 비디오 선택 & 상세 화면

--- Language/react/02. create-react-app/Dream Coding/react-basic/07. youtube/src/app.jsx
+++ Language/react/02. create-react-app/Dream Coding/react-basic/07. youtube/src/app.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 const App = ({ youTube }) => {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selecteVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
     youTube
@@ -17,10 +23,26 @@ const App = ({ youTube }) => {
       .mostPopular() //
       .then((videos) => setVideos(videos));
   }, []);
+
+  console.log(selectedVideo ? 'list' : 'grid');
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selecteVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
     </div>
   );
 };
