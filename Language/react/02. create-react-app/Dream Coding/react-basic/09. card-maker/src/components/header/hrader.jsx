7be469095534a03e43fe1537b560700b5fe4bd78
박성언 <tjddjs90@naver.com>
2020-10-08 19:07:33 +0900

    YouTube modify

--- Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/header/hrader.jsx
+++ Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/header/hrader.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from './header.module.css';
 
-const Header = (props) => <div>Header</div>;
+const Header = (props) => <header></header>;
 
 export default Header;
