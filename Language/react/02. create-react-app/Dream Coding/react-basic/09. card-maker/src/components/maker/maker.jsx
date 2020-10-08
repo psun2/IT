3e9e9c7f9fb4f996e33a57bc8a615c557c7a3a28
박성언 <tjddjs90@naver.com>
2020-10-08 22:39:27 +0900

    10.7 Maker 헤더와 푸터 💡

--- Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/maker/maker.jsx
+++ Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/maker/maker.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import Editor from '../editor/editor';
 import Footer from '../footer/footer';
 import Header from '../header/hrader';
+import Preview from '../preview/preview';
 import styles from './maker.module.css';
 const Maker = ({ authService }) => {
   const history = useHistory();
@@ -22,6 +24,10 @@ const Maker = ({ authService }) => {
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
