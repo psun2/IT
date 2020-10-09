a67333f5b5132b6da52ad8601b3725981de0e3f8
박성언 <tjddjs90@naver.com>
2020-10-09 17:01:59 +0900

    10.15 State 업데이트하기 💡(중요 내용 📔)

--- Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/card_edit_form/card_edit_form.jsx
+++ Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/card_edit_form/card_edit_form.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { memo, useRef } from 'react';
 import Button from '../button/button';
 import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
-const CardEditForm = ({ card }) => {
+const CardEditForm = memo(({ card, updateCard, deleteCard }) => {
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
   const {
     name,
     company,
@@ -15,28 +22,70 @@ const CardEditForm = ({ card }) => {
     fileURL,
   } = card;
 
-  const onSubmit = () => {};
+  const onChange = (event) => {
+    if (event.currentTarget == null) return;
+    event.preventDefault();
+    console.log(event.currentTarget.name);
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+  const onSubmit = () => {
+    deleteCard(card);
+  };
 
   return (
     <form className={styles.form}>
-      <input className={styles.input} type="text" name="name" value={name} />
       <input
+        ref={nameRef}
+        className={styles.input}
+        type="text"
+        name="name"
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        ref={companyRef}
         className={styles.input}
         type="text"
         name="company"
         value={company}
+        onChange={onChange}
       />
-      <select className={styles.select} name="theme" value={theme}>
+      <select
+        ref={themeRef}
+        className={styles.select}
+        name="theme"
+        value={theme}
+        onChange={onChange}
+      >
         <option value="light">Light</option>
         <option value="dark">Dark</option>
         <option value="colorful">Colorful</option>
       </select>
-      <input className={styles.input} type="text" name="title" value={title} />
-      <input className={styles.input} type="text" name="email" value={email} />
+      <input
+        ref={titleRef}
+        className={styles.input}
+        type="text"
+        name="title"
+        value={title}
+        onChange={onChange}
+      />
+      <input
+        ref={emailRef}
+        className={styles.input}
+        type="text"
+        name="email"
+        value={email}
+        onChange={onChange}
+      />
       <textarea
+        ref={messageRef}
         className={styles.textarea}
         name="message"
         vlaue={message}
+        onChange={onChange}
       ></textarea>
       <div className={styles.fileInput}>
         <ImageFileInput />
@@ -44,6 +93,6 @@ const CardEditForm = ({ card }) => {
       <Button name="Delete" onClick={onSubmit} />
     </form>
   );
-};
+});
 
 export default CardEditForm;
--- Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/maker/maker.jsx
+++ Language/react/02. create-react-app/Dream Coding/react-basic/09. card-maker/src/components/maker/maker.jsx
@@ -8,11 +8,14 @@ import Header from '../header/hrader';
 import Preview from '../preview/preview';
 import styles from './maker.module.css';
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    // 배열의 형태를 유지하면 ...
+    // URUD가 꼬일 뿐 더러...
+    // 그렇게 하지 않으면 모든 배열의 내용을 복사해야 합니다....
+    1: {
       id: '1',
       name: 'Park',
-      company: 'ssibal',
+      company: 'none',
       theme: 'dark',
       title: 'Software Engineer',
       email: 'tjddjs90@gmail.com',
@@ -20,10 +23,10 @@ const Maker = ({ authService }) => {
       fileName: 'Park',
       fileURL: null,
     },
-    {
+    2: {
       id: '2',
       name: 'Park2',
-      company: 'ssibal',
+      company: 'none',
       theme: 'light',
       title: 'Software Engineer',
       email: 'tjddjs90@gmail.com',
@@ -31,10 +34,10 @@ const Maker = ({ authService }) => {
       fileName: 'Park',
       fileURL: 'park.png',
     },
-    {
+    3: {
       id: '3',
       name: 'Park3',
-      company: 'ssibal',
+      company: 'none',
       theme: 'colorful',
       title: 'Software Engineer',
       email: 'tjddjs90@gmail.com',
@@ -42,10 +45,10 @@ const Maker = ({ authService }) => {
       fileName: 'Park',
       fileURL: null,
     },
-    {
+    4: {
       id: '4',
       name: 'Park3',
-      company: 'ssibal',
+      company: 'none',
       theme: 'colorful',
       title: 'Software Engineer',
       email: 'tjddjs90@gmail.com',
@@ -53,10 +56,10 @@ const Maker = ({ authService }) => {
       fileName: 'Park',
       fileURL: null,
     },
-    {
+    5: {
       id: '5',
       name: 'Park3',
-      company: 'ssibal',
+      company: 'none',
       theme: 'colorful',
       title: 'Software Engineer',
       email: 'tjddjs90@gmail.com',
@@ -64,7 +67,7 @@ const Maker = ({ authService }) => {
       fileName: 'Park',
       fileURL: null,
     },
-  ]);
+  });
   const history = useHistory();
 
   const onLogout = () => {
@@ -80,16 +83,45 @@ const Maker = ({ authService }) => {
       });
   });
 
-  const addCard = (card) => {
-    const update = [...cards, card];
-    setCards(update);
+  const createOrUpdateCard = (card) => {
+    // const updated = { ...cards };
+    // updated[card.id] = card;
+    // setCards(updated); // 이렇게 할 수 도있으나,
+    // 비동기적으로 처리 하는 부분이 있으면 항상 저 state의 값이 최신
+    // 의 상태라고 말 할 수 없습니다.
+    // setState 는 콜백 할수를 받을 수 있으므로 함수로 처리함을 권장 드립니다.
+
+    setCards((cards) => {
+      // 인자로 받아오는 cards 라는 state는
+      // setCards 가 불린 바로 그 시점에서의 state입니다.
+      const updated = { ...cards }; // 인자로 받아온 카드를 복사하여
+      // 다음 로직을 진행합니다.
+      updated[card.id] = card;
+      return updated;
+    });
+  };
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      // 인자로 받아오는 cards 라는 state는
+      // setCards 가 불린 바로 그 시점에서의 state입니다.
+      const updated = { ...cards }; // 인자로 받아온 카드를 복사하여
+      // 다음 로직을 진행합니다.
+      delete updated[card.id];
+      return updated;
+    });
   };
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
