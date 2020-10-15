c24aaeb942f9e094ec2e5cd89f31e082d4c24667
박성언 <tjddjs90@naver.com>
2020-10-15 12:04:28 +0900

    02. 01. Sass

---
+++ Language/react/02. create-react-app/벨로퍼트/react-tutorial/styling-with-sass/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from './components/Button';
+import './App.scss';
+
+const App = (props) => (
+  <div className="App">
+    <div className="buttons">
+      <Button>BUTTON</Button>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ Language/react/02. create-react-app/벨로퍼트/react-tutorial/styling-with-sass/src/components/Button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = ({ children }) => <button className="Button">{children}</button>;
+
+export default Button;
