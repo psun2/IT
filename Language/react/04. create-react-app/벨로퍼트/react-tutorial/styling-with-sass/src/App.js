fd38e7c22c7775a22b09140bc683ad0be452d345
박성언 <tjddjs90@naver.com>
2020-10-16 01:02:19 +0900

    03. 02. Sass 버튼 B - color props 설정하기(mixin 과 include)

--- Language/react/04. create-react-app/벨로퍼트/react-tutorial/styling-with-sass/src/App.js
+++ Language/react/04. create-react-app/벨로퍼트/react-tutorial/styling-with-sass/src/App.js
@@ -10,6 +10,30 @@ const App = (props) => (
       <Button>BUTTON</Button>
       <Button size="small">BUTTON</Button>
     </div>
+    <div className="buttons">
+      <Button color="gray" size="large">
+        BUTTON
+      </Button>
+      <Button color="gray" size="medium">
+        BUTTON
+      </Button>
+      <Button color="gray">BUTTON</Button>
+      <Button color="gray" size="small">
+        BUTTON
+      </Button>
+    </div>
+    <div className="buttons">
+      <Button color="pink" size="large">
+        BUTTON
+      </Button>
+      <Button color="pink" size="medium">
+        BUTTON
+      </Button>
+      <Button color="pink">BUTTON</Button>
+      <Button color="pink" size="small">
+        BUTTON
+      </Button>
+    </div>
   </div>
 );
 
--- Language/react/04. create-react-app/벨로퍼트/react-tutorial/styling-with-sass/src/components/Button.js
+++ Language/react/04. create-react-app/벨로퍼트/react-tutorial/styling-with-sass/src/components/Button.js
@@ -2,14 +2,17 @@ import React from 'react';
 import classNames from 'classnames';
 import './Button.scss';
 
-const Button = ({ children, size }) => (
+// size: large, medium, small
+// color: blue, pink, gray
+const Button = ({ children, size, color }) => (
   // <button className={['Button', size].join(' ')}>{children}</button>
   // <button className={`Button ${size}`}>{children}</button>
-  <button className={classNames('Button', size)}>{children}</button>
+  <button className={classNames('Button', size, color)}>{children}</button>
 );
 
 Button.defaultProps = {
   size: 'medium',
+  color: 'blue',
 };
 
 export default Button;
