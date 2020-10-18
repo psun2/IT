ac34c70dce20ee3971cfecbb2cf70eab7b6a41d7
박성언 <tjddjs90@naver.com>
2020-10-18 15:40:09 +0900

    03. 02. useReducer 로 요청 상태 관리하기

--- Language/react/04. create-react-app/벨로퍼트/04. react-api/api-integrate/src/Users.js
+++ Language/react/04. create-react-app/벨로퍼트/04. react-api/api-integrate/src/Users.js
@@ -1,35 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+/*
+  LOADING
+  SUCCESS
+  ERROR
+*/
+function reducer(state, action) {
+  switch (action.type) {
+    case 'LOADING':
+      return { ...state, loading: true };
+    case 'SUCCESS':
+      return { ...state, loading: false, data: action.data, error: null };
+    case 'ERROR':
+      return { ...state, loading: false, data: null, error: action.error };
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 const Users = (props) => {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, {
+    loading: false,
+    data: null,
+    error: null,
+  });
 
   const fetchUsers = async () => {
+    // 로딩 시작
+    dispatch({ type: 'LOADING' });
     try {
-      setUsers(null);
-      setError(null);
-      setLoading(true);
       const response = await axios.get(
         'https://jsonplaceholder.typicode.com/users/',
       );
-      setUsers(response.data);
+      dispatch({ type: 'SUCCESS', data: response.data });
     } catch (error) {
       console.log(error.response);
       console.log(error.response.status);
-      setError(error);
+      dispatch({ type: 'ERROR', error });
     }
-    setLoading(false);
   };
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
+  const { loading, error, data: users } = state;
+
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다/</div>;
-  if (!users) return null;
+  if (!users) return null; // null 리턴시 공백 화면 이 보입니다.
 
   return (
     <>
