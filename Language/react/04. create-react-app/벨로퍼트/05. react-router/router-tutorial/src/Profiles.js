489faf0b50b59b38326dda2890ae027c18eb6a91
박성언 <tjddjs90@naver.com>
2020-10-18 21:54:54 +0900

    04. 03. 서브라우트

---
+++ Language/react/04. create-react-app/벨로퍼트/05. react-router/router-tutorial/src/Profiles.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Profile from './Profile';
+import { Link, Route } from 'react-router-dom';
+
+const Profiles = (props) => (
+  <div>
+    <h3>사용자 목록</h3>
+    <ul>
+      <li>
+        <Link to="/profiles/park">park</Link>
+      </li>
+      <li>
+        <Link to="/profiles/hommer">hommer</Link>
+      </li>
+    </ul>
+    <div>사용자를 선택해주세요.</div>
+    {/* <Route
+      path="/profiles"
+      exact
+      render={() => <div>사용자를 선택해주세요.</div>}
+    /> */}
+    <Route path="/profiles/:username" component={Profile} />
+  </div>
+);
+
+export default Profiles;
