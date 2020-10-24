5091e83672e26b3498aea5996bb08a5887c4f262
박성언 <tjddjs90@naver.com>
2020-10-24 21:38:12 +0900

    14. 13. 사용자 경험 개선 C - createPromiseThunkById

--- Language/react/04. create-react-app/벨로퍼트/06. react-Redux/learn-redux-middleware/src/modules/posts.js
+++ Language/react/04. create-react-app/벨로퍼트/06. react-Redux/learn-redux-middleware/src/modules/posts.js
@@ -5,7 +5,9 @@
 import * as postsAPI from '../api/posts';
 import {
   createPromiseThunk,
+  createPromiseThunkById,
   handleAsnycActions,
+  handleAsnycActionsById,
   reducerUtils,
 } from '../lib/asyncUtils';
 
@@ -20,15 +22,16 @@ const GET_POST_ERROR = 'post/GET_POST_ERROR';
 const CLEAR_POST = 'CLEAR_POST';
 
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST, meta: id });
-  try {
-    const payload = await postsAPI.getPostById(id);
-    dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
-  } catch (error) {
-    dispatch({ type: GET_POST_ERROR, payload: error, error: true, meta: id });
-  }
-};
+// export const getPost = (id) => async (dispatch) => {
+//   dispatch({ type: GET_POST, meta: id });
+//   try {
+//     const payload = await postsAPI.getPostById(id);
+//     dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
+//   } catch (error) {
+//     dispatch({ type: GET_POST_ERROR, payload: error, error: true, meta: id });
+//   }
+// };
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 export const clearPost = () => ({ type: CLEAR_POST });
 
 const initialState = {
@@ -37,37 +40,39 @@ const initialState = {
 };
 
 const getPostsReducer = handleAsnycActions(GET_POSTS, 'posts', true);
-const getPostReducer = (state, action) => {
-  const id = action.meta;
-  switch (action.type) {
-    case GET_POST:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
-        },
-      };
-    case GET_POST_SUCCESS:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.success(action.payload),
-        },
-      };
-    case GET_POST_ERROR:
-      return {
-        ...state,
-        post: {
-          ...state.post,
-          [id]: reducerUtils.error(action.payload),
-        },
-      };
-    default:
-      return state;
-  }
-};
+// const getPostReducer = (state, action) => {
+//   const id = action.meta;
+//   switch (action.type) {
+//     case GET_POST:
+//       return {
+//         ...state,
+//         post: {
+//           ...state.post,
+//           [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
+//         },
+//       };
+//     case GET_POST_SUCCESS:
+//       return {
+//         ...state,
+//         post: {
+//           ...state.post,
+//           [id]: reducerUtils.success(action.payload),
+//         },
+//       };
+//     case GET_POST_ERROR:
+//       return {
+//         ...state,
+//         post: {
+//           ...state.post,
+//           [id]: reducerUtils.error(action.payload),
+//         },
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+const getPostReducer = handleAsnycActionsById(GET_POST, 'post', true);
 
 export default function posts(state = initialState, action) {
   switch (action.type) {
